06fa7c3da21c1f989b31a05b0cc20defdfe542a5
svolodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2024-11-13 19:32:17 +0000

    Deploying to gh-pages from @ svolodarskyi/goit-js-hw-11@fb427cf04f5c72bc76eaaa388634c704eacbd146 🚀

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import{S as f,i}from"./assets/vendor-BrddEoy-.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();function m(n){return n.map(({webformatURL:o,largeImageURL:t,tags:s,likes:e,views:r,comments:a,downloads:p})=>`
+import{S as p,i}from"./assets/vendor-BrddEoy-.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();function m(s){return s.map(({webformatURL:o,largeImageURL:t,tags:n,likes:e,views:r,comments:a,downloads:u})=>`
         <li class="photo-card">
     <a href="${t}">
     <img class="photo-card-img"
     src="${o}"
-    alt="${s}"
+    alt="${n}"
     />
 </a>
 <div class="info">
@@ -21,9 +21,9 @@ import{S as f,i}from"./assets/vendor-BrddEoy-.js";(function(){const o=document.c
         </p>
         <p class="info-item">
             <span>Downloads</span>
-            ${p}
+            ${u}
         </p>
     </div>
 </li>
-`).join("")}function d(n,o){o.insertAdjacentHTML("beforeend",n);let t=new f(".gallery a",{caption:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250});t.refresh(),t.on("show.simplelightbox")}const g="46939525-73ecae044838d7dfbbfadb664",h="https://pixabay.com/api/";function y(n){const o=new URLSearchParams({key:g,q:n,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${h}?${o}`).then(t=>{if(!t.ok)throw new Error(t.statusText);return t.json()})}const c=document.querySelector(".search-field"),L=document.querySelector("button"),l=document.querySelector(".gallery"),u=document.querySelector(".loader-icon-container");L.addEventListener("click",b);function b(n){n.preventDefault(),l.innerHTML="",u.classList.toggle("loader");const o=c.value.trim();y(o).then(t=>{if(t.hits===0)i.error({message:'"Sorry, there are no images matching your search query. Please try again!"',position:"topRight"});else{const e=m(t.hits);d(e,l)}}).catch(t=>{i.error({message:t.message,position:"topRight"})}).finally(()=>{u.classList.toggle("loader"),c.value=""})}
+`).join("")}function f(s,o){o.insertAdjacentHTML("beforeend",s);let t=new p(".gallery a",{caption:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250});t.refresh(),t.on("show.simplelightbox")}const d="46939525-73ecae044838d7dfbbfadb664",g="https://pixabay.com/api/";function h(s){const o=new URLSearchParams({key:d,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${g}?${o}`).then(t=>{if(!t.ok)throw new Error(t.statusText);return t.json()})}const y=document.querySelector(".searching-form");document.querySelector("button");const c=document.querySelector(".gallery"),l=document.querySelector(".loader-icon-container");y.addEventListener("submit",b);function b(s){s.preventDefault(),c.innerHTML="";const o=s.target.elements.search.value.trim();if(!o){i.error({message:"Please enter a keyword into the search field",position:"topRight"});return}l.classList.toggle("loader"),h(o).then(t=>{if(console.log("asda"),t.hits.length===0)i.error({message:'"Sorry, there are no images matching your search query. Please try again!"',position:"topRight"});else{const e=m(t.hits);f(e,c)}}).catch(t=>{i.error({message:t.message,position:"topRight"})}).finally(()=>{l.classList.toggle("loader"),s.target.elements.search.value=""})}
 //# sourceMappingURL=index.js.map
